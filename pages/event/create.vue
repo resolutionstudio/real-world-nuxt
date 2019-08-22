8bdce494c37d16995f848472594428839674c182
Lihat Ervin <office@resolution-studio.ro>
2019-08-22 15:29:07 +0300

    Before uploading to Heroku or resoro server

---
+++ pages/event/create.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>+ Create an Event</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create New Event',
+      meta: []
+    }
+  }
+}
+</script>
